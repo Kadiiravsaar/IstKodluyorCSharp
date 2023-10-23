13c24b9f79290958a2122dcc8026e353726d8dde
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-23 16:44:38 +0300

    Ders5 integer veri tipi

--- Ders5-IntVeriTipi/Program.cs
+++ Ders5-IntVeriTipi/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Value type - değer tip
+
+int number1 = 10; // -2147483648 ile 2147483647 arasındaki sayılardan oluşabilir.
+Console.WriteLine("Number is {0}",number1);
