dafbeeb50098d38f6f7ad715788fa9a25759466b
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-23 16:48:41 +0300

    long ,short ve byte veri tipi

--- Ders6Long/Program.cs
+++ Ders6Long/Program.cs
@@ -1,4 +1,13 @@
-﻿// int 2 katı değer kaplar
+﻿// int 2 katı değer kaplar -- int 32 bit
 // bellekte 2 katı yer kapladık
-long number1 = 1555866; // -9.223.372.036.854.775.808 - 9.223.372.036.854.775.807
+long number1 = 1555866; // -9.223.372.036.854.775.808 - 9.223.372.036.854.775.807 -- 64 bit
 Console.WriteLine("Number is {0}", number1);
+
+// short 
+
+short number2 = 32767; // -32768 - 32768// 16 bitlik yer tutar
+
+
+// byte 8 bitlik yer kaplar
+
+byte number3 = 15;//0,254 arası değer
