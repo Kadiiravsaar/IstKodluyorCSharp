90f1c6f00d0f8dd4b3089c786678ae1a87956e27
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-23 16:44:47 +0300

    ders 6 long veri tipi

---
+++ Ders6Long/Program.cs
@@ -0,0 +1,4 @@
+﻿// int 2 katı değer kaplar
+// bellekte 2 katı yer kapladık
+long number1 = 1555866; // -9.223.372.036.854.775.808 - 9.223.372.036.854.775.807
+Console.WriteLine("Number is {0}", number1);
