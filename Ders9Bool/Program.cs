00ec20fbf44115e68f67919785f35c9952377a92
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-23 16:55:14 +0300

    Bool, char, double, decimal veri tipleri

---
+++ Ders9Bool/Program.cs
@@ -0,0 +1,18 @@
+﻿// bool mantıksal veri tipidir true ya da false gibi
+bool condition = false;
+
+// char
+
+char character = 'X';
+Console.WriteLine("Karakter {0}",character); // char
+Console.WriteLine("Karakter {0}",(int)character); // ascii de sayısal karşılığı
+
+
+// double
+
+double number1 = 10.45;
+
+
+// decimal
+
+decimal number2 = 10.46m;
