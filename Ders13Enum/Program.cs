5d5ef72264453c24e69ab2fdebd5817d452c9211
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-23 17:00:53 +0300

    Enum

---
+++ Ders13Enum/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Friday {0}",(int)Days.Friday);
+
+// Monday (Pazar) 0
+enum Days
+{
+    Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+}
+
+// eğer monday = 10 dersek sırayla devam eder mesela Tuesday 11 olur
+
+
+
+
