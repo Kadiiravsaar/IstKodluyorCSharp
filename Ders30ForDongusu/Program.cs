378c8a3b3868f3cc07aaf3bccb4b062bcf508c52
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-24 16:15:48 +0300

    Ders 30 31

---
+++ Ders30ForDongusu/Program.cs
@@ -0,0 +1,20 @@
+﻿for (int i =1000; i >=0; i = i-2)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("Finished");
+
+
+#region Ders31 While
+
+int number = 100;
+while (number>=0)
+{
+    Console.WriteLine(number); // şuan sürekli döner bu yüzden number'ı küçültmek gerek
+    number--;
+}
+
+
+
+
+#endregion
