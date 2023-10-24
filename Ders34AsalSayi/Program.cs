f71bde115e20ded374af6cdebb435dc8b21dc82e
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-24 17:01:28 +0300

    Ders 34 asal

---
+++ Ders34AsalSayi/Program.cs
@@ -0,0 +1,56 @@
+﻿PrimeNumber primeNumber = new PrimeNumber();
+
+if (primeNumber.IsPrimeNumber(6))
+{
+    Console.WriteLine("This is a prime number");
+}
+else
+{
+    Console.WriteLine("This is a not prime number");
+}
+
+
+// *************************************** böyle de olur ***********////////////////////
+//if (IsPrimeNumber(6))
+//{
+//    Console.WriteLine("This is a prime number");
+//}
+//else
+//{
+//    Console.WriteLine("This is a not prime number");
+//}
+
+
+//static bool IsPrimeNumber(int number)
+//{
+//    bool result = true;
+//    for (int i = 2; i < number; i++)
+//    {
+//        if (number % i == 0)
+//        {
+//            result = false;
+//            break;
+//        }
+//    }
+//    return result;
+//}
+
+
+class PrimeNumber
+{
+    public bool IsPrimeNumber(int number)
+    {
+        bool result = true;
+        for (int i = 2; i < number; i++)
+        {
+            if (number%i==0)
+            {
+                result = false; 
+                break;
+            }
+        }
+        return result;
+    }
+
+}
+
