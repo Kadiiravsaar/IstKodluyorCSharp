9076e8b7c2cdbf58b79fc24581a928bade186d9d
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-24 13:12:47 +0300

    Ders 15,16,17 (If blokları)

---
+++ Ders15IfBloklari/Program.cs
@@ -0,0 +1,41 @@
+﻿var number = 50;
+if (number == 50)
+{
+    Console.WriteLine("Number is 10");
+}
+else
+{
+    Console.WriteLine("Number is not 10");
+
+}
+
+#region 16
+
+// Ders 16 start
+
+Console.WriteLine(number == 10 ? "number is 10" : "number is not 10"); // single line if
+
+// Ders 16 end
+#endregion
+
+#region 17
+// Ders 17 start
+
+if (number == 50)
+{
+    Console.WriteLine("Number is 10");
+}
+else if (number == 10)
+{
+    Console.WriteLine("Number is not 10");
+
+}
+else
+{
+    Console.WriteLine("???");
+}
+
+
+// Ders 17 end
+
+#endregion
