16528b8b6e9d296888c7bf4788deda8f6a5af011
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-24 16:11:01 +0300

    Ders 29 çok boyutlu diziler

---
+++ Ders29CokBoyutluDiziler/Program.cs
@@ -0,0 +1,17 @@
+﻿string[,] regions = new string[5, 3]
+{
+    {"İst","İzmit","Balıkesir"},
+    {"Ankara","Konya","Kırıkkale"},
+    {"Antalya","Adana","Mersin"},
+    {"Rize","Trabzon","Samsun"},
+    {"İZmir","Muğla","Manisa"},
+};
+
+for (int i = 0; i <= regions.GetUpperBound(0); i++)
+{
+    for (int j = 0; j <= regions.GetUpperBound(1); j++)
+    {
+        Console.WriteLine(regions[i,j]);
+    }
+    Console.WriteLine("*******");
+}
