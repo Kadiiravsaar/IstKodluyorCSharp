f73b88648606fd8b0aed562f4e4d757a6b43a68f
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-24 15:42:00 +0300

    Ders 28 arrays

---
+++ Ders28Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] students = new string[3];
+students[0] = "Engin";
+students[1] = "Kadir";
+students[2] = "Salih";
+
+foreach (var item in students)
+{
+    Console.WriteLine(item);
+}
+
+
+// yazım yolları 
+string[] students2 = new[] {"Engin","Kadir"};
+string[] students3 = {"Engin","Kadir"};
+string[] students4 = new string[] {"Kadir","Engin"};
