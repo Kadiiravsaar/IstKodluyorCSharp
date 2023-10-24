b67d7bd425a33cc6c0582d235f7f9a58548341bd
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-24 13:15:52 +0300

    Ders 18 switch

---
+++ Ders18Switch/Program.cs
@@ -0,0 +1,12 @@
+﻿var number = 10;
+switch (number)
+{
+	case 0:
+        Console.WriteLine("Number is 0");
+        break;
+    case 10:
+        Console.WriteLine("Number is 10");
+        break;
+    default:
+		break;
+}
