90e8a22a9c2cd1268854d0417d199c505bb45d2a
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-24 15:37:33 +0300

    Ders 26 metot overloading ve 27 params

---
+++ Ders26MethodOverloading/Program.cs
@@ -0,0 +1,63 @@
+﻿//static int Multiply(int num1, int num2) // (int num1,int num2) metodun imzasızıdr
+//{
+//    return num1 * num2;
+//}
+
+//static int Multiply(int num1, int num2, int num3)
+//{
+//    return num1 * num2 * num3;
+//}
+
+//Console.WriteLine(Multiply(2, 4));
+//Console.WriteLine(Multiply(2, 4, 7));
+
+
+#region Metot overload
+Overload overload = new Overload();
+var sum1 = overload.AddNumbers(10, 20);
+Console.WriteLine(sum1);
+
+
+
+#endregion
+
+
+
+#region Params
+Console.WriteLine(ParamsOver.Add(120, 30, 45));
+
+#endregion
+
+
+// params parametrenin son parametresi olmak zorundadır
+
+public class Overload
+{
+    public int AddNumbers(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public double AddNumbers(int num1, int num2, int num3)
+    {
+        return num1 + num2 + num3;
+    }
+}
+
+
+#region Ders 27 params
+
+
+
+
+public class ParamsOver
+{
+    public static int Add(params int[] numbers)
+    {
+        return numbers.Sum();
+    }
+
+    
+}
+
+#endregion
