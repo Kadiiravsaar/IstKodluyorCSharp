0f7cfc37fa994513625a33a063a432560425b649
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-24 13:24:02 +0300

    Ders 19-20

---
+++ Ders19Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿var number = 50;
+if (number>=0 && number<=100)
+{
+    Console.WriteLine("Number is between 0-100");
+}
+else if (number >= 50 && number <= 150)
+{
+    Console.WriteLine("Number is between 50-150");
+}
+else
+{
+    Console.WriteLine("Number.....");
+}
+
+
+#region Ders 20
+
+if (number<100)
+{
+    if (number>=90 && number<95)
+    {
+        Console.WriteLine("Number is between 90-100");
+    }
+}
+
+
+#endregion
