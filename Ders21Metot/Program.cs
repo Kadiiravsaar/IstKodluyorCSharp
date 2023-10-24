b0f3f35e02b698cc4b552f2fa3de5a9e2f5133f3
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-24 14:24:15 +0300

    Ders 21,22,23 (Metotlar)

---
+++ Ders21Metot/Program.cs
@@ -0,0 +1,40 @@
+﻿static void Add()
+{
+    Console.WriteLine("Added!!");
+}
+Add();
+
+// ********************************************************//
+
+#region Ders 22 (parametreli)
+
+static int Add2(int num1, int num2)
+{
+    var result = num1 + num2;
+    return result;
+}
+Console.WriteLine(Add2(12, 13));
+// ya da 
+
+var result = Add2(12, 13);
+Console.WriteLine(result);
+
+#endregion
+
+// ********************************************************//
+
+#region Ders 23 (Default parametre)
+
+// default değerler son metotda olmalı
+static int Add3(int num1, int num2=30)
+{
+    var result = num1 + num2;
+    return result;
+}
+Console.WriteLine(Add3(12, 13));
+// ya da 
+
+var result2 = Add3(12);
+Console.WriteLine(result2);
+
+#endregion
