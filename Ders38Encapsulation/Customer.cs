179e61595944b0bc3e480a3766fd65b15fe158f7
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-25 16:39:26 +0300

    Ders 39 Encapsulation

---
+++ Ders38Encapsulation/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders38Encapsulation
+{
+    public class Customer
+    {
+        private string _firstName { get; set; }
+        public int Id { get; set; }
+        public string FirstName {
+            get 
+            {
+                return "Mrs." + _firstName;
+            }
+            set 
+            {
+                _firstName = value;
+            }
+        }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
---
+++ Ders38Encapsulation/Program.cs
@@ -0,0 +1,11 @@
+﻿using Ders38Encapsulation;
+
+Customer customer1 = new Customer
+{
+    Id = 2,
+    City = "Istanbul",
+    FirstName = "Kadir",
+    LastName = "Avsar"
+};
+
+Console.WriteLine(customer1.FirstName);
