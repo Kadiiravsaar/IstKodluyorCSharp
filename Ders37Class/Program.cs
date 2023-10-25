a1f7920fdec6dfbc6a4278fb5794dd42a8262b0f
kadiiravsaar <kadiiravsar@gmail.com>
2023-10-25 16:12:46 +0300

    Ders 37 38

--- Ders37Class/Program.cs
+++ Ders37Class/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿CustomerManager customerManager = new CustomerManager();
+customerManager.Add();
+customerManager.Update();
+
+ProductManager productManager = new ProductManager();
+productManager.Add();
+productManager.Update();
+
+Customer customer = new Customer();
+customer.Id = 1;
+customer.City = "Istanbul";
+customer.FirstName = "Kadir";
+customer.LastName = "Avsar";
+
+
+Customer customer1 = new Customer
+{
+    Id=2,City="Istanbul",FirstName="Kadir",LastName="Avsar"
+};
+
+Console.WriteLine(customer1.FirstName);
